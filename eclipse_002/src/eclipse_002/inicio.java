1d0edd9d50fde28441e85d34f6a4e8589502287f
Tebez Ariel <Tebez Ariel@SonyPC>
2016-09-10 10:41:13 -0300

    original

---
+++ eclipse_002/src/eclipse_002/inicio.java
@@ -0,0 +1,12 @@
+package eclipse_002;
+
+public class inicio {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		System.out.println("anda");
+		
+	}
+
+}
